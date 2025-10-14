7e436c87705d71a6f24404c6678073ad86e49325
Wang Hang <1154690309@qq.com>
2025-10-14 15:22:55 +0800

    Initial commit

---
+++ LiteS001/main.py
@@ -0,0 +1,39 @@
+import mujoco
+import mujoco.viewer
+import threading
+
+def run_viewer(model, data):
+    with mujoco.viewer.launch_passive(model, data) as viewer:
+        while viewer.is_running():
+            mujoco.mj_step(model, data)
+            viewer.sync()
+
+def main():
+
+    type = input("Enter '0' for right hand, '1' for left hand, '2' for both: ")
+    
+    match(type):
+        case '0':
+            model_r = mujoco.MjModel.from_xml_path('model/rohand_lites_right.xml')
+            data_r = mujoco.MjData(model_r)
+            run_viewer(model_r, data_r)
+        case '1':
+            model_l = mujoco.MjModel.from_xml_path('model/rohand_lites_left.xml')
+            data_l = mujoco.MjData(model_l)
+            run_viewer(model_l, data_l)
+        case '2':
+            model_r = mujoco.MjModel.from_xml_path('model/rohand_lites_right.xml')
+            data_r = mujoco.MjData(model_r)
+            model_l = mujoco.MjModel.from_xml_path('model/rohand_lites_left.xml')
+            data_l = mujoco.MjData(model_l)
+
+            t1 = threading.Thread(target=run_viewer, args=(model_r, data_r))
+            t2 = threading.Thread(target=run_viewer, args=(model_l, data_l))
+
+            t1.start()
+            t2.start()
+            t1.join()
+            t2.join()
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,10 @@
+# ROHAND MUJOCO PACKAGE
+
+A collection of ROHand MuJoCo package
+
+[A001&A002](./A001&A002/README.md): MuJoCo package for ROH-A001 and ROH-A002
+
+[AP001](./AP001/README.md): MuJoCo package for ROH-AP001
+
+[LiteS001](./LiteS001/README.md): MuJoCo package for LiteS-001
+"# rohand_mujoco" 
